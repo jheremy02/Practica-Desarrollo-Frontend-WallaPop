fdf30b2b58d5403f7344faeeec42dc7b28a9ec5e
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-05 19:44:20 -0500

    inicio el proyecto realzando la vista de anuncio

---
+++ AdvertisementsView.js
@@ -0,0 +1,20 @@
+
+export function buildAdvertisementItem(advertisement) {
+    let advertisementTemplate = `
+    <div class="advertisement-item">
+    <div class="card" style="width: 18rem;">
+        <img src="${advertisement.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${advertisement.name}</h5>
+          <h6 >Description</h6>
+          <p class="card-text">${advertisement.description}</p>
+          <h6>Price</h6>
+          <p>${advertisement.price}</p>
+          <h6>Type</h6>
+          <p>${advertisement.type}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    `
+}
