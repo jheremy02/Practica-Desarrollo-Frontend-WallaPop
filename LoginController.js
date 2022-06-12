c8f1ee3fab3f6376f8829e88f44444c3321ad00f
Jheyson Menéndez <jheremy26@gmail.com>
2022-06-11 22:08:15 -0500

    agrego codigo para el controlador vista y modelo de la seccion de login

---
+++ LoginController.js
@@ -0,0 +1,58 @@
+
+import { signUpService } from "./SignUpService.js";
+
+export class LoginController{
+    constructor(loginFormElement){
+
+        this.loginFormElement=loginFormElement
+        this.main()
+
+    }
+
+    main(){
+        this.onAnyInputChanged()
+        this.onSubmitForm()
+    }
+
+    onAnyInputChanged () {
+        const inputElements=Array.from(this.loginFormElement.querySelectorAll('input'))
+        inputElements.forEach(element=>{
+            element.addEventListener('input',()=>{
+                this.checkIfAllInputsAreFilled(inputElements)
+            })
+        })
+     }
+
+     checkIfAllInputsAreFilled(inputElements){
+        const areAllInputsFilled = inputElements.every(element=>element.value)
+
+        if (areAllInputsFilled) {
+            this.loginFormElement.querySelector("button").removeAttribute("disabled");
+
+        } else {
+            this.loginFormElement.querySelector("button").setAttribute("disabled",'');
+        }
+     }
+
+      onSubmitForm(){
+         this.loginFormElement.addEventListener('submit',(event)=>{
+             event.preventDefault();
+             const formData=new FormData(this.loginFormElement)
+             const username=formData.get('inputUsername')
+             const password=formData.get('inputPassword')
+
+             this.loginUser(username,password)
+
+         })
+     }
+
+     async loginUser(username,password) {
+         try {
+            await signUpService.loginUser(username,password)
+         } catch (error) {
+             console.log(error)
+         }
+        
+     }
+
+}
---
+++ login.js
@@ -0,0 +1,10 @@
+
+import { LoginController } from "./LoginController.js";
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const loginFormElement=document.querySelector('form')
+    const loginController=new LoginController(loginFormElement)
+
+})
